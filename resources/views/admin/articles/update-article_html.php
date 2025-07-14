3e917be0484f1262b0cbfe1c5d1f8e316f854695
ROSTAND LELE <rostoinfocus@gmail.com>
2025-07-14 19:23:31 +0100

    police et design

--- resources/views/admin/articles/update-article_html.php
+++ resources/views/admin/articles/update-article_html.php
@@ -1,41 +1,48 @@
+
 <h1>Éditer un article</h1>
 
+<div style="background-color: white; padding: 10px;">
 
-<form method="POST" action="update-article.php?id=<?= $articleId ?>" enctype="multipart/form-data">
-    <input type="hidden" name="id" value="<?= $articleId ?>">
-    
-    <div class="form-group">
-        <label for="title">Titre :</label>
-        <input type="text" name="title" id="title" value="<?= htmlspecialchars($title ?? '') ?>" required>
-    </div>
-    
-    <div class="form-group">
-        <label for="introduction">Introduction :</label>
-        <textarea name="introduction" id="introduction" required><?= htmlspecialchars($introduction ?? '') ?></textarea>
-    </div>
-    
-    <div class="form-group">
-        <label for="content">Contenu :</label>
-        <textarea name="content" id="content" required><?= htmlspecialchars($content ?? '') ?></textarea>
-    </div>
-    
-    <div class="form-group">
-        <label for="a_image">Image de l'article :</label>
-        <input type="file" name="a_image" id="a_image" accept="image/*">
+
+    <form method="POST" action="update-article.php?id=<?= $articleId ?>" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?= $articleId ?>">
         
-        <?php if (!empty($currentImage)): ?>
-            <div class="current-image">
-                <img src="<?= $currentImage ?>" alt="Image actuelle" style="max-width: 200px;">
-                <label>
-                    <input type="checkbox" name="delete_image" value="1">
-                    Supprimer cette image
-                </label>
-            </div>
-        <?php endif; ?>
-    </div>
+        <div class="form-control">
+            <label for="title">Titre :</label>
+            <input type="text" name="title" id="title" value="<?= htmlspecialchars($title ?? '') ?>" required>
+        </div>
+        
+        <div class="form-control">
+            <label for="introduction">Introduction :</label>
+            <textarea name="introduction" id="introduction" required><?= htmlspecialchars($introduction ?? '') ?></textarea>
+        </div>
+        
+        <div class="form-control">
+            <label for="content">Contenu :</label>
+            <textarea name="content" id="content" required><?= htmlspecialchars($content ?? '') ?></textarea>
+        </div>
+        
+        <div class="form-control">
+            <label for="a_image">Image de l'article :</label>
+            <input type="file" name="a_image" id="a_image" accept="image/*">
+            
+            <?php if (!empty($currentImage)): ?>
+                <div class="current-image">
+                    <img src="<?= $currentImage ?>" alt="Image actuelle" style="max-width: 200px;">
+                    <label>
+                        <input type="checkbox" name="delete_image" value="1">
+                        Supprimer cette image
+                    </label>
+                </div>
+            <?php endif; ?>
+        </div>
+        
     
-    <div class="form-actions">
-        <input type="submit" name="update" value="Mettre à jour" class="btn primary">
-        <a href="admin.php" class="btn secondary">Retour à l'administration</a>
-    </div>
-</form>
+
+       
+        <div class="form-control" style="display: flex;">
+            <button type="submit" name="update" value="Mettre à jour" >Mettre à jour </button>
+            <a href="admin.php" class="btn secondary" >Retour à l'administration</a>  
+        </div>
+    </form>
+</div>
--- resources/views/layouts/admin-layout/layout_html.php
+++ resources/views/layouts/admin-layout/layout_html.php
@@ -10,6 +10,7 @@ if (session_status() == PHP_SESSION_NONE) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='/resources/css/boxicons.min.css' rel='stylesheet'>
 	<link rel="stylesheet" href="/resources/css/style.css">
+	<link rel="stylesheet" href="/resources/css/add-update.css">
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<title>Stage Admin-blog PHP 2025 - <?= htmlspecialchars($pageTitle) ?></title>
 </head>
